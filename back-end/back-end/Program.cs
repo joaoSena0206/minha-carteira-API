d0c4977f1904bf99ff0a3f62c7157d55ef3e22be
João Sena <joaosena0206@gmail.com>
2025-04-01 12:08:08 -0300

    feat: mostra status code 422 em vez de 400, para melhor mostrar erro de validação

--- back-end/back-end/Program.cs
+++ back-end/back-end/Program.cs
@@ -1,12 +1,26 @@
 using back_end.Data;
 using back_end.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var problemDetailsFactory = context.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+            ValidationProblemDetails problemDetails = problemDetailsFactory.CreateValidationProblemDetails(context.HttpContext, context.ModelState, 422);
+
+            return new ObjectResult(problemDetails)
+            {
+                StatusCode = 422
+            };
+        }
+    );
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
